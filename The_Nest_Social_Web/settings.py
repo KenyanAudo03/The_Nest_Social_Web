e5c99ea2703c0aef8c01302915d734c7211b60a3
Victor Owino <vo4685336@gmail.com>
2025-02-13 17:00:44 +0300

    refactor settings.py to clean up static files configuration

--- The_Nest_Social_Web/settings.py
+++ The_Nest_Social_Web/settings.py
@@ -121,15 +121,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-# Ensure Django can find static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 # Media files (User-uploaded content)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
