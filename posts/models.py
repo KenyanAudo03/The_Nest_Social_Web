03a099b82e81389b30ca40ca8f0fcf74fe5606bc
Victor Owino <vo4685336@gmail.com>
2025-02-21 06:22:46 +0300

    feat: add repost, like, and view functionality to Post model

--- posts/models.py
+++ posts/models.py
@@ -15,9 +15,18 @@ class Post(models.Model):
         blank=True,
         related_name="reposted_posts",
     )
+    repost_count = models.PositiveIntegerField(default=0)
+    like_count = models.PositiveIntegerField(default=0)
+    view_count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"Post by {self.user.username} - {self.created_at}"
+
+    def increment_repost_count(self):
+        """Increment repost count for the original post"""
+        if self.reposted_from:
+            self.reposted_from.repost_count += 1
+            self.reposted_from.save()
 
 
 class Media(models.Model):
@@ -26,3 +35,32 @@ class Media(models.Model):
 
     def __str__(self):
         return f"Media for Post ID {self.post.id}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes"
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "post")  # Ensure a user can like a post only once
+
+    def __str__(self):
+        return f"{self.user.username} liked Post {self.post.id}"
+
+
+class View(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="views",
+        null=True,
+        blank=True,
+    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="views")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"View on Post {self.post.id}"
