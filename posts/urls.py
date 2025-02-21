77ae4be9ce4c88d9fde716eab3556ade3ee6a14b
Victor Owino <vo4685336@gmail.com>
2025-02-21 12:13:19 +0300

    feat: implement user login functionality with email verification and redirect to home

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = "posts"
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login/", views.user_login, name="user_login"),
     path("create/", views.create_post, name="create_post"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,17 +4,57 @@ from django.contrib.auth.decorators import login_required
 from .models import Post, Media
 from .forms import PostForm, MediaUploadForm
 from django.forms import modelformset_factory
+from django.contrib.auth import authenticate, login as auth_login
+from django.middleware.csrf import get_token
+from django.views.decorators.cache import never_cache
+from django.contrib import messages
+from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 
 # Create your views here.
+
+# Login
+@never_cache
+def user_login(request):
+    if request.method == "POST":
+        email = request.POST.get("username", "").strip()
+        password = request.POST.get("password", "").strip()
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            if not user.email_verified:
+                messages.error(
+                request,
+                mark_safe(f'Email not verified. <a href="{reverse("resend_email", args=[email])}">Click here</a> to verify.')
+                )
+                return render(request, "login.html")
+
+            auth_login(request, user)
+            return redirect("posts:home")
+        else:
+            messages.error(request, "Invalid email or password.")
+            return render(request, "login.html")
+
+    csrf_token = get_token(request)
+    response = render(request, "login.html", {"csrf_token": csrf_token})
+    response["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response["Pragma"] = "no-cache"
+    response["Expires"] = "0"
+
+    return response
+
 def home(request):
+    if not request.user.is_authenticated:
+        return redirect("user_login")  # Redirects to the login page
+    
     posts = (
         Post.objects.select_related("user")
         .prefetch_related("media")
         .order_by("-created_at")
     )
     return render(request, "home.html", {"posts": posts})
-
 
 @login_required
 def create_post(request):
